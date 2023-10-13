57d8c4a2ea31ea7c0320de73bf3f84f45c6cfd2d
mrush12 <mrush12@ivytech.edu>
2023-10-12 21:11:52 -0400

    change

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  console.log(req.url);
+
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
+  
+  const greet = _.once(() => {
+    console.log('hello');
+});
+greet();
+greet();
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
-
-  // res.write('<p>hello, ninjas</p>');
-  // res.write('<p>hello again, ninjas</p>');
-  // res.end();
-
-  // send html file
-  // fs.readFile('./views/index.html', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.end();
-  //   }
-  //   //res.write(data);
-  //   res.end(data);
-  // });
 
   // routing
   let path = './views/';
@@ -39,9 +34,9 @@ const server = http.createServer((req, res) => {
       res.end();
       break;
     default:
-      path += '404.html';
-      res.statusCode = 404;
-  }
+        path += '404.html';
+        res.statusCode = 404;
+    }
 
   // send html
   fs.readFile(path, (err, data) => {
@@ -60,8 +55,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000');
 });
-
-
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000')
-})
